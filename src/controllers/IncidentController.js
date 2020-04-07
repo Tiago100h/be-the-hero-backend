da5d6d40fabcd575be395da427837b0602590c6d
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2020-04-07 19:39:50 -0300

    Corrigir contador de incidentes

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -4,7 +4,7 @@ module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query;
 
-        const [count] = await connection('incidents').count();
+        const [{ count }] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
@@ -19,7 +19,7 @@ module.exports = {
                 'ongs.uf'
             ]);
 
-        response.header('X-Total-Count', count['count(*)']);
+        response.header('X-Total-Count', count);
 
         return response.json(incidents);
     },
